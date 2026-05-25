49889d27dfdcae7366c14befd1eca3d51e12ddc9
Allison Satoro <satoroal@wgc.school.nz>
2026-05-25 17:31:44 +1200

    Created basic fields for the pet

---
+++ VirtualPet.java
@@ -0,0 +1,31 @@
+import ecs100.*;
+import java.awt.Color;
+/**
+ * Create a virtual pet
+ *
+ * @Allison Satoro
+ * 25/05/2026
+ */
+public class VirtualPet
+{
+    // Fields needed for the virtual pet
+    private String name; // Pet's name
+    
+    // Pet statistics
+    private int hunger;
+    private int energy;
+    private int happiness;
+    
+    // Pet visuals
+    private double petX;
+    private double petY;
+    private double petWidth;
+    private double petHeight;
+    
+    /**
+     * Constructor for objects of class virtualpet
+     */
+    public VirtualPet() {
+        
+    }
+}
